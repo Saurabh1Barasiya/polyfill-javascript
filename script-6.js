d89c9fd3277599ab1d2be8fc8e9dc44cefb624d0
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2025-12-17 19:30:32 +0530

    polyfill of call().

--- script-6.js
+++ script-6.js
@@ -1,26 +1,27 @@
-// polyfill for flat.
+// polyfill of call().
 
-Array.prototype.myFlat = function(depth){
-    if(depth === undefined){
-        depth = 1;  // because flat() function me default depth 1 hoti h
-    }
+// call() kisi function par lagta h.
 
-    let result = [];
-    for(let element of this){
-        if(Array.isArray(element) && depth > 0){
-            result.push(...element.myFlat(depth-1))
-        }else{
-            result.push(element);
-        }
-    }
-
-    return result;
+function myFunc(address){
+    console.log(`Name is ${this.name}`);
+    console.log(`Name is ${this.age}`);
+    console.log(`Address is ${address}`);
 }
 
-arr = [10,20,[30,40],[50,60,[70,80,90,[100,200]]]];
-// console.log(arr);
-// const flateArray = arr.flat();
-// console.log(flateArray);
+const obj = {
+    name:"saurabh",
+    age:25,
+}
 
-const flateData = arr.myFlat(2);
-console.log(flateData);
+// myFunc.call(obj,"harrai");
+
+Function.prototype.myCall = function(context={},...args){
+    if(typeof this !== "function"){
+        throw new Error("calling object is not function");
+    }
+
+    context.fn = this;
+    context.fn(...args);
+}
+
+myFunc.myCall(obj,"harrai");
